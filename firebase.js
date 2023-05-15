79949edc1c1f6adfa682cc95d05d29503ea04a85
yurisilva <yurikgs@outlook.com>
2023-05-15 08:14:07 -0300

    FEAT: firebase and firestore added

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAREx4xnE2nxyiYNscr3Hol4LMNR19wR5c",
+  authDomain: "react-test-124e2.firebaseapp.com",
+  projectId: "react-test-124e2",
+  storageBucket: "react-test-124e2.appspot.com",
+  messagingSenderId: "264868803166",
+  appId: "1:264868803166:web:0fc56f8f42f9546a5efacf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
